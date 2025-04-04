0bb31c3865ae88eccefdd4762bc17e7cf2557ca0
AcoMat <aaco.maty@gmail.com>
2025-04-03 22:26:20 -0300

    Add framework

---
+++ src/main/java/unq/pdes/_5/g1/segui_tus_compras/SeguiTusComprasGrupo1Application.java
@@ -0,0 +1,13 @@
+package unq.pdes._5.g1.segui_tus_compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguiTusComprasGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguiTusComprasGrupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/unq/pdes/_5/g1/segui_tus_compras/SeguiTusComprasGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package unq.pdes._5.g1.segui_tus_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguiTusComprasGrupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
