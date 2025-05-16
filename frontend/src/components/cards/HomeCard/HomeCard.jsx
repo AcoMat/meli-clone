262abdf114713133c4f5b76db2d01570e058779e
AcoMat <aaco.maty@gmail.com>
2025-05-16 20:44:46 -0300

    Refactor(Home): Failed attemp of change the home carrousel

--- frontend/src/components/cards/HomeCard/HomeCard.jsx
+++ frontend/src/components/cards/HomeCard/HomeCard.jsx
@@ -3,15 +3,15 @@ export default function HomeCard({ data, navigate }) {
 
     return (
         <article
-        className={`bg-body d-flex flex-column h-100 w-100 p-3 rounded text-decoration-none text-dark home-card`} 
+        className={`bg-body d-flex flex-column rounded text-decoration-none w-50 text-dark home-card`} 
         onClick={() => navigate(data.link)}
         style={{cursor: "pointer"}}
         >
-            <span className="fw-medium">{data.header}</span>
-            <img className='img-fluid w-75 my-2 mx-auto' src={data.img} />
+            <span className="fw-medium p-2 text-center">{data.header}</span>
+            <img className='img-fluid w-50 my-2 mx-auto' src={data.img} />
             <div className="d-flex flex-column justify-content-between h-100 text-center">
-                <span className='card-desc' style={{ fontSize: "14px" }}>{data.desc}</span>
-                <span className='fw-medium border bg-primary-subtle text-primary rounded' style={{ fontSize: "12px" }}>{data.footer}</span>
+                <span className='card-desc p-2' style={{ fontSize: "12px" }}>{data.desc}</span>
+                <span className='fw-medium border bg-primary-subtle text-primary rounded m-2' style={{ fontSize: "12px" }}>{data.footer}</span>
             </div>
         </article>
     )
